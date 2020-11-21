21bee233e7889541d735445bc0b9823dbc0475cb
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2020-11-21 13:55:36 +0000

    Changed ' to " for consistency

--- sign_lang_translator/translator/trained.py
+++ sign_lang_translator/translator/trained.py
@@ -16,17 +16,17 @@ directory = "sign_lang_translator/translator/"
 train_df = pd.read_csv(f"{directory}asl_dataset/asl_test.csv")
 test_df = pd.read_csv(f"{directory}asl_dataset/asl_test.csv")
 test = pd.read_csv(f"{directory}asl_dataset/asl_test.csv")
-y = test['label']
+y = test["label"]
 train_df.head()
 
 # plt.figure(figsize = (10,10)) # Label Count
 # sns.set_style("darkgrid")
-# sns.countplot(train_df['label'])
+# sns.countplot(train_df["label"])
 
-y_train = train_df['label']
-y_test = test_df['label']
-del train_df['label']
-del test_df['label']
+y_train = train_df["label"]
+y_test = test_df["label"]
+del train_df["label"]
+del test_df["label"]
 
 label_binarizer = LabelBinarizer()
 y_train = label_binarizer.fit_transform(y_train)
@@ -45,12 +45,10 @@ x_train = x_train.reshape(-1,28,28,1)
 x_test = x_test.reshape(-1,28,28,1)
 ###
 
-
 # Load pre trained model
 model = load_model(f"{directory}pre_trained/model.h5")
 print("Loaded model from disk")
 
-
-model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=["accuracy"])
 accuracy = model.evaluate(x_test,y_test)[1]*100
 print(f"Accuracy --- {accuracy:.2f}%")
