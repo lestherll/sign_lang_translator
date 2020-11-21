cff54d2c8a97f11fb2ec1860b1e265e67f008782
Lesther Llacuna (UG) <c0026034@newcastle.ac.uk>
2020-11-21 22:37:44 +0000

    Added comments

--- sign_lang_translator/translator/trained.py
+++ sign_lang_translator/translator/trained.py
@@ -9,43 +9,35 @@ from keras.models import Sequential
 
 from sklearn.preprocessing import LabelBinarizer
 
-
+# dir to translator folder
 directory = "sign_lang_translator/translator/"
 
-#load train data
-train_df = pd.read_csv(f"{directory}asl_dataset/asl_test.csv")
+# load test data
 test_df = pd.read_csv(f"{directory}asl_dataset/asl_test.csv")
 test = pd.read_csv(f"{directory}asl_dataset/asl_test.csv")
+
+# preprocessing
 y = test["label"]
-train_df.head()
-
-
-y_train = train_df["label"]
 y_test = test_df["label"]
-del train_df["label"]
 del test_df["label"]
 
 label_binarizer = LabelBinarizer()
-y_train = label_binarizer.fit_transform(y_train)
 y_test = label_binarizer.fit_transform(y_test)
 
-x_train = train_df.values
+# return a numpy representation
 x_test = test_df.values
 
 # Normalize the data
-x_train = x_train / 255
 x_test = x_test / 255
 
-# cnn requires 3 inputs
 # reshape data into 3d
-x_train = x_train.reshape(-1,28,28,1)
 x_test = x_test.reshape(-1,28,28,1)
-###
 
 # Load pre trained model
 model = load_model(f"{directory}pre_trained/model.h5")
 print("Loaded model from disk")
 
+# compile and test
 model.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=["accuracy"])
 accuracy = model.evaluate(x_test,y_test)[1]*100
 print(f"Accuracy --- {accuracy:.2f}%")
